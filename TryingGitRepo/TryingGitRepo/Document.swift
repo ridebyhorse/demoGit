fcced9b0b9fe5707558dc9d269a9f9598ee3b035
Maria Nesterova <ridebyhorse@gmail.com>
2023-10-07 19:51:07 +0300

    Initial commit

---
+++ TryingGitRepo/TryingGitRepo/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  TryingGitRepo
+//
+//  Created by Мария Нестерова on 07.10.2023.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
